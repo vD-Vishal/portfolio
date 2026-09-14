c8a86428e18176e1146a4563dd8332279ec2257b
Vishal Durga DR <dev.vishaldurga@gmail.com>
2026-09-15 04:52:03 +0530

    Change base URL in Vite config to root

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/portfolio/',
+  // base: '/portfolio/',
+  base:'/',
   plugins: [react(), tailwindcss()],
 });
